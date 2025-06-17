54e91ef9cec49978ed15476d58d66030267e60de
temesgendemeke <tdemeke36@gmail.com>
2025-06-17 15:35:19 +0300

    update hero section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full mt-10  ">
+    <footer className="w-full mt-10">
       <div className="flex justify-center my-4">
-      <p className="text-center">
+      <p className="text-center ">
         © {new Date().getFullYear()} Slango. Developed by{" "}
         <Link
         href="https://github.com/temesgendemeke"
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,10 +7,10 @@ import { Plus, Search } from "lucide-react";
 const HeroSection = () => {
   return (
     <div className="flex justify-center  flex-col items-center gap-4 mt-10 md:mt-20 w-full">
-      <h1 className="text-center text-5xl lg:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r dark:from-slate-50 dark:to-slate-300 from-black to-gray-700">
+      <h1 className="text-center text-2xl md:text-5xl lg:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r dark:from-slate-50 dark:to-slate-300 from-black to-gray-700">
         DISCOVER, SHARE, DEFINE SLANG
       </h1>
-      <p className="dark:text-gray-300 text-black text-center text-sm md:text-lg max-w-sm md:max-w-2xl">
+      <p className="dark:text-gray-300 text-black text-center text-[0.7rem] md:text-lg max-w-xs md:max-w-2xl">
         The community driven platform for sharing and discover the latest slang
       </p>
       <div className="flex gap-2 flex-col md:flex-row ">
