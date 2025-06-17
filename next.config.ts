a5f144075892746ea9e4d67a7ed2c21dec2d2cf5
temesgendemeke <tdemeke36@gmail.com>
2025-06-17 16:18:21 +0300

    add next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   poweredByHeader: false,
   images: {
     domains: ["res.cloudinary.com", "asset.cloudinary.com"],
