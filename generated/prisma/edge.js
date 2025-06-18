03ba9694075d6d73b610e8cef9ca988f8515bb06
temesgendemeke <tdemeke36@gmail.com>
2025-06-18 10:37:58 +0300

    update: vercel.json

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -237,6 +237,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mongodb",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -238,6 +238,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mongodb",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
